3223215ccff09158fbf6445000ef3bb34a8e9c10
joonseok kim <mansukim1125@gmail.com>
2025-02-09 01:56:41 +0900

    feat(task-manager): 작업 조회 기능 추가

--- app/src/main/java/com/example/demo/application/TaskManager.java
+++ app/src/main/java/com/example/demo/application/TaskManager.java
@@ -1,12 +1,14 @@
 package com.example.demo.application;
 
 import com.example.demo.domain.Task;
+import com.example.demo.domain.TaskStatus;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TaskManager {
 
-    private final ArrayList<Task> tasks;
+    private final List<Task> tasks;
 
     public TaskManager() {
         this.tasks = new ArrayList<>();
@@ -17,6 +19,14 @@ public class TaskManager {
     }
 
     public Task createTask(String title, int priority) {
+
+    }
+
+    public Task getTaskById(String id) {
+
+    }
+
+    public List<Task> getTasksByStatus(TaskStatus status) {
 
     }
 }
