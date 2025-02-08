58425f7f71dc48f9ae1b807d65813bd2ea9ccc4a
joonseok kim <mansukim1125@gmail.com>
2025-02-09 01:49:35 +0900

    feat(task-manager): task-manager 생성자 정의

---
+++ app/src/main/java/com/example/demo/application/TaskManager.java
@@ -0,0 +1,14 @@
+package com.example.demo.application;
+
+import com.example.demo.domain.Task;
+
+import java.util.ArrayList;
+
+public class TaskManager {
+
+    private final ArrayList<Task> tasks;
+
+    public TaskManager() {
+        this.tasks = new ArrayList<>();
+    }
+}
