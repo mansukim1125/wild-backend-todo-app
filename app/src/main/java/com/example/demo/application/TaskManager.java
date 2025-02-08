7f89773194f6ff789b1402c8ce8bf80f568d544b
joonseok kim <mansukim1125@gmail.com>
2025-02-09 02:06:16 +0900

    feat(task-manager): 작업 상태 변경 기능 추가

--- app/src/main/java/com/example/demo/application/TaskManager.java
+++ app/src/main/java/com/example/demo/application/TaskManager.java
@@ -30,9 +30,9 @@ public class TaskManager {
 
     }
 
-    public void changeTaskStatus(String id, TaskStatus status) {
+    public Task changeTaskStatus(String id, TaskStatus status) {
 
     }
 
-
+    
 }
